730f16b0558b2284e8adf0c9a5cce5d1a99c9a6e
Alexander Danilchenko <39950413+OlexanderD@users.noreply.github.com>
2019-03-09 13:29:34 +0200

    Update additional functions.h

--- additional functions.h
+++ additional functions.h
@@ -6,5 +6,5 @@ size_t getOption(); // get the option if the menu is called
 bool fillByFile(dynamicArray& a, const string& inputFilePath) noexcept;
 bool fillByKbd(dynamicArray& a);
 bool isNumber(const string& s); // checks if number consists of numeric elements
-bool isPrimeNum(const int& k); // checks if if number is prime or not
+bool isPrimeNum(const int& k); // checks if the number is prime or not
 void outputArray(const dynamicArray& a, const string& outputFilePath);
